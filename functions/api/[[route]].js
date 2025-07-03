4dd69cc6ff5d80d06da9373092af4175b26cf285
Aadiprofessional <aadiprofessional8@gmail.com>
2025-07-03 20:02:58 +0530

    Fix: Update function handler context and add health route

--- functions/api/[[route]].js
+++ functions/api/[[route]].js
@@ -1,4 +1,6 @@
 import app from '../../src/app.js';
 
 // Cloudflare Pages Functions expects named exports for HTTP methods
-export const onRequest = app.fetch; 
+export const onRequest = async (context) => {
+  return app.fetch(context.request, context.env, context);
+}; 
